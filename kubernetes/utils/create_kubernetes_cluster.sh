620eae406abc0361fc0762575a524353e02f5de6
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-20 23:47:12 +0200

    ci(kubernetes): add label when creating cluster too

--- kubernetes/utils/create_kubernetes_cluster.sh
+++ kubernetes/utils/create_kubernetes_cluster.sh
@@ -1,5 +1,6 @@
 source "./logger.sh"
 source "./build_docker_image.sh"
+source "./update_version_label_in_kubernetes_deployment.sh"
 
 function create_namespaces() {
   ENTER
@@ -17,6 +18,7 @@ function create_express_kubernetes_example_resources() {
   ENTER
   build_docker_image "express-kubernetes-example" "development"
   kubectl apply -f ../apps/express-kubernetes-example/deployment.yaml -n development
+  update_version_label_in_kubernetes_deployment "express-kubernetes-example" "development"
   kubectl apply -f ../apps/express-kubernetes-example/service.yaml -n development
   kubectl apply -f ../apps/express-kubernetes-example/hpa.yaml -n development
   EXIT
