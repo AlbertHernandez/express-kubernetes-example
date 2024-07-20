08b54008c5a7ef6dc7e13de8b8d5b721524c4ef9
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-20 23:50:46 +0200

    ci(kubernetes): add console log

--- src/app/health/api/health-controller.ts
+++ src/app/health/api/health-controller.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from "http-status-codes";
 
 export class HealthController {
   run(req: Request, res: Response) {
+    console.log("hello");
     res.status(StatusCodes.OK).send();
   }
 }
