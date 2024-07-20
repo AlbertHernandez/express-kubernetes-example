e60558ff2572e36eaf616b669140d809b47c73c0
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-20 15:27:07 +0200

    ci(kubernetes): log health received

--- src/app/health/api/health-controller.ts
+++ src/app/health/api/health-controller.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { Logger } from "@/shared/logger/logger.ts";
+
 export class HealthController {
+  private readonly logger;
+
+  constructor(dependencies: { logger: Logger }) {
+    this.logger = dependencies.logger;
+  }
+
   run(req: Request, res: Response) {
+    this.logger.info("Received request to check health");
     res.status(StatusCodes.OK).send();
   }
 }
--- src/app/health/api/health-router.ts
+++ src/app/health/api/health-router.ts
@@ -1,10 +1,15 @@
 import express from "express";
 
+import { ConsoleLogger } from "@/shared/logger/console-logger.ts";
+
 import { HealthController } from "./health-controller";
 
 const healthRouter = express.Router();
 
-const healthController = new HealthController();
+const logger = new ConsoleLogger();
+const healthController = new HealthController({
+  logger,
+});
 
 healthRouter.get("/", healthController.run.bind(healthController));
 
