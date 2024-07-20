79fb372829dca551690cbae359aef485692f4cc3
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-20 23:53:51 +0200

    ci(kubernetes): remove log

--- src/app/health/api/health-controller.ts
+++ src/app/health/api/health-controller.ts
@@ -3,7 +3,6 @@ import { StatusCodes } from "http-status-codes";
 
 export class HealthController {
   run(req: Request, res: Response) {
-    console.log("hello 2");
     res.status(StatusCodes.OK).send();
   }
 }
