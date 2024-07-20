9bc037802e895e46f0f618bb547a61b9bd70056f
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-20 18:30:59 +0200

    ci(kubernetes): change file structure for kubernetes folder

--- kubernetes/utils/deploy_kubernetes_apps.sh
+++ kubernetes/utils/deploy_kubernetes_apps.sh
@@ -1,36 +1,24 @@
 source "../utils/logger.sh"
 
-function build_docker_images() {
+function configure_hpa_tools() {
+  ENTER
+  kubectl apply -f ./metrics-server/component-v0.7.1.yaml
+  EXIT
+}
+
+function create_express_kubernetes_example_resources() {
   ENTER
   docker build -t express-kubernetes-example -f ../../Dockerfile ../../
-  EXIT
-}
-
-function start_deployments() {
-  ENTER
-  kubectl apply -f ../deployments/express-kubernetes-example.yaml
-  EXIT
-}
-
-function start_services() {
-  ENTER
-  kubectl apply -f ../services/express-kubernetes-example.yaml
-  EXIT
-}
-
-function configure_hpas() {
-  ENTER
-  kubectl apply -f ../hpas/metrics-server/component-v0.7.1.yaml
-  kubectl apply -f ../hpas/express-kubernetes-example.yaml
+  kubectl apply -f ../apps/express-kubernetes-example/deployment.yaml
+  kubectl apply -f ../apps/express-kubernetes-example/service.yaml
+  kubectl apply -f ../apps/express-kubernetes-example/hpa.yaml
   EXIT
 }
 
 function main() {
   ENTER
-  build_docker_images
-  start_deployments
-  start_services
-  configure_hpas
+  configure_hpa_tools
+  create_express_kubernetes_example_resources
   EXIT
 }
 
