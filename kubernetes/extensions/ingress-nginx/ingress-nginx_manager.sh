9d3933331890fe2f5708fdc044983ab89949fb54
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-21 15:38:01 +0200

    ci(kubernetes): remove nginx admission

--- kubernetes/extensions/ingress-nginx/ingress-nginx_manager.sh
+++ kubernetes/extensions/ingress-nginx/ingress-nginx_manager.sh
@@ -5,5 +5,6 @@
 function create_ingress_nginx() {
   ENTER
   kubectl apply -f ./kubernetes/extensions/ingress-nginx/nginx-v1.11.1.yaml
+  kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
   EXIT
 }
