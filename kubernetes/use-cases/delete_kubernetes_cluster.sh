4431cbdf54bd3c0ea3599f4f916c09555ccd4b53
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-21 15:35:29 +0200

    ci(kubernetes): delete app resources

--- kubernetes/use-cases/delete_kubernetes_cluster.sh
+++ kubernetes/use-cases/delete_kubernetes_cluster.sh
@@ -1,11 +1,13 @@
 source "./kubernetes/utils/logger.sh"
 source "./kubernetes/utils/local_hosts_manager.sh"
 source "./kubernetes/extensions/metallb/metallb_manager.sh"
+source "./kubernetes/apps/express-kubernetes-example/delete_resources.sh"
 
 function delete_all_resources() {
   ENTER
   kubectl delete all --all --all-namespaces
   delete_metallb_resources
+  delete_express_kubernetes_example_resources
   EXIT
 }
 
